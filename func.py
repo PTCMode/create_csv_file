8a52411be3219d5585211f0ab970b916fde0fa23
PTCMode <1044871170@qq.com>
2022-02-11 16:50:09 +0800

    First Commit.

---
+++ func.py
@@ -0,0 +1,192 @@
+#!/usr/bin/python3
+
+import time
+import math
+import random
+import platform
+import config
+
+
+class execOpt:
+    'execOpt'
+    def __init__(self):
+        # 设置 是否使用config 默认值
+        self.usingConfig = False
+        # 设置 是否合并多线程产生文件 默认值
+        self.needMergeFiles = False
+        # 设置 是否打乱文件内的行 默认值
+        self.needShufFiles = False
+        # 设置 默认线程数量
+        self.threadCount = 1
+        # 设置 默认文件路径
+        if platform.architecture()[1] == 'ELF':
+            self.filePath = './tmp_file'
+        else:
+            self.filePath = '.\\tmp_file'
+        # 设置 默认分隔符
+        self.separator = ';'
+        # 生成 随机数 (系统时间基准)
+        self.rand = str(time.time()).split(".")[-1]
+        self.seed = int(-1)
+
+    def create_col(self, array):
+        if array[0].upper() == 'NULL':
+            return null(array)
+        elif array[0].upper() == 'INT' \
+                or array[0].upper() == 'LONG' \
+                or array[0].upper() == 'DOUBLE' \
+                or array[0].upper() == 'NUMBER':
+            return int_order(array)
+        elif array[0].upper() == 'CHAR' \
+                or array[0].upper() == 'VARCHAR':
+            return char_order(array)
+        elif array[0].upper() == 'DATE' \
+                or array[0].upper() == 'TIMESTAMP':
+            return date_order(array)
+        elif array[0].upper() == 'YMINTERVAL' \
+                or array[0].upper() == 'YM':
+            return y2m_order(array)
+        elif array[0].upper() == 'DSINTERVAL' \
+                or array[0].upper() == 'DS':
+            return d2s_order(array)
+        if array[0].upper() == 'CUSTOM':
+            return custom(array)
+        else:
+            return
+
+
+class func:
+    'parent class'
+
+    def __init__(self, array:list):
+        # 初始化成员变量
+        self.val = array[1]
+        self.optNumFuncVec = []
+        self.optStrFuncVec = []
+        self.keyArray = []
+        # 解析特殊处理
+        if len(array) > 2:
+            for index in range(2, len(array)):
+                if array[index][0].upper() == 'LIMIT':
+                    self.optNumFuncVec.append(lambda dividend : dividend % array[index][1])
+                elif array[index][0].upper() == 'KEY':
+                    self.add_option_key(array[index])
+                elif array[index][0].upper() == 'RANDOM':
+                    self.add_option_random(array[index])
+                else:
+                    pass
+            for index in range(2, len(array)):
+                if array[index][0].upper() == 'EXLEN':
+                    self.add_option_exlen(array[index])
+                elif array[index][0].upper() == 'QUOTE':
+                    pass
+                else:
+                    pass
+
+    def add_option_key(self, optDesc:list):
+        sum = 0
+        tmp = 0
+        if len(optDesc) > 2:
+            for keyRateIdx in range(2, len(optDesc)):
+                sum = sum + optDesc[keyRateIdx]
+                if keyRateIdx < optDesc[1] + 2:
+                    tmp = tmp + config.rowCount * optDesc[keyRateIdx]
+                elif keyRateIdx == optDesc[1] + 2:
+                    tmp = config.rowCount
+                self.keyArray.append(math.floor(tmp))
+                pass
+        if len(optDesc) - 2 < optDesc[1]:
+            rate = (1 - sum) / (optDesc[1] - len(optDesc) + 2)
+            for keyRateIdx in range(len(optDesc) - 2, optDesc[1]):
+                if keyRateIdx < optDesc[1] - 1:
+                    tmp = tmp + config.rowCount * rate
+                else:
+                    tmp = config.rowCount
+                self.keyArray.append(math.floor(tmp))
+                pass
+            pass
+        # print(self.keyArray)
+
+        def tmpFunc(val):
+            for vecIdx in range(len(self.keyArray)):
+                if val < self.keyArray[vecIdx]:
+                    return vecIdx
+            return
+
+        self.optNumFuncVec.append(tmpFunc)
+        pass
+
+    def add_option_random(self, optDesc:list):
+
+        def tmpFunc(val):
+            return random.randint(0, optDesc[1] - 1)
+
+        self.optNumFuncVec.append(tmpFunc)
+        pass
+
+    def add_option_exlen(self, optDesc:list):
+
+        def tmpFunc(val:str):
+            return val.rjust(optDesc[1], ' ')
+
+        self.optStrFuncVec.append(tmpFunc)
+        pass
+
+class null(func):
+    'null'
+    def next(self, val:int):
+        return ''
+
+class int_order(func):
+    'int_order'
+    def next(self, val:int):
+        tmp = val
+        for optNumFunc in self.optNumFuncVec:
+            tmp = optNumFunc(tmp)
+        return str(self.val + tmp)
+
+class char_order(func):
+    'char_order'
+    def next(self, val:int):
+        tmp = val
+        for optNumFunc in self.optNumFuncVec:
+            tmp = optNumFunc(tmp)
+        tmp = str(self.val + tmp)
+        for optStrFunc in self.optStrFuncVec:
+            tmp = optStrFunc(tmp)
+        return str('\"' + tmp + '\"')
+
+class date_order(func):
+    'date_order'
+    def next(self, val:int):
+        tmp = val
+        for optNumFunc in self.optNumFuncVec:
+            tmp = optNumFunc(tmp)
+        return time.strftime("\"%Y-%m-%d %H:%M:%S\"", time.localtime(self.val + tmp))
+
+class y2m_order(func):
+    'y2m_order'
+    def next(self, val:int):
+        tmp = val
+        for optNumFunc in self.optNumFuncVec:
+            tmp = optNumFunc(tmp)
+        tmp = self.val + tmp
+        return str('\"' + str((tmp) // 12) + '-' + str((tmp) % 12) + '\"')
+
+class d2s_order(func):
+    'd2s_order'
+    def next(self, val:int):
+        tmp = val
+        for optNumFunc in self.optNumFuncVec:
+            tmp = optNumFunc(tmp)
+        tmp = self.val + tmp
+        return str("\"" + str((tmp) // 86400) + time.strftime(" %H:%M:%S\"", time.localtime(tmp)))
+
+class custom(func):
+    'custom'
+    def next(self, val:int):
+        tmp = val
+        for optNumFunc in self.optNumFuncVec:
+            tmp = optNumFunc(tmp)
+        return self.val[tmp % len(self.val)]
+
---
+++ run.py
@@ -0,0 +1,227 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import random
+import getopt
+import multiprocessing
+import threading
+import platform
+
+import config
+import func
+
+
+def usage():
+    print('==================== help ====================')
+    print('Nothing here now.')
+    print('==================== end  ====================')
+
+
+class craete:
+    'craete csv data by ordered value.'
+
+    def __init__(self, execOptList):
+        # TODO: 下方三个列表合并成一个类来管理
+        # 线程对象列表
+        self.threadList = []
+        # 文件路径列表
+        self.fileList = []
+        # 列生成类对象列表
+        self.colList = []
+        # 生成行数
+        self.rowCount = int(1)
+        # 生成列数
+        self.colCount = int(1)
+        # 配置选项信息
+        self.execOptList = execOptList
+        # 选择初始化方式
+        if (self.execOptList.usingConfig):
+            self.init_by_conf()
+        else:
+            self.init_by_user()
+
+    def init_by_conf(self):
+        print('==================== init_by_conf ====================')
+        print('读取 config.py 文件')
+        if (len(config.colInfoList) != config.colCount):
+            print('请检查 config.py 文件格式')
+            sys.exit()
+        if (config.rowCount <= 0):
+            sys.exit()
+        if self.execOptList.threadCount > config.rowCount:
+            print("线程数大于总行数")
+            sys.exit()
+        self.rowCount = config.rowCount
+        for colInfoIdx in range(len(config.colInfoList)):
+            print('\n生成第 %d 列信息:' % (colInfoIdx + 1))
+            print('  - 第 %d 列类型: %s\n  - 第 %d 列数据设置: ' % ( \
+                    colInfoIdx + 1, config.colInfoList[colInfoIdx][0], colInfoIdx + 1), end = '')
+            if isinstance(config.colInfoList[colInfoIdx][1], list):
+                print(' | '.join(map(str, config.colInfoList[colInfoIdx][1])))
+            else:
+                print(config.colInfoList[colInfoIdx][1])
+
+            curCol = self.execOptList.create_col(config.colInfoList[colInfoIdx])
+            if (curCol == None):
+                print('请检查 config.py 文件格式')
+                sys.exit()
+            self.colList.append(curCol)
+
+    # TODO: 修改更新手动初始化逻辑
+    def init_by_user(self):
+        print('==================== init_by_user ====================')
+        self.colCount = int(input('请输入生成列数: '))
+        self.rowCount = int(input('\n请输入生成行数: '))
+        if self.execOptList.threadCount > self.rowCount:
+            print("线程数大于总行数")
+            sys.exit()
+        print('')
+        #
+        index = 1
+        while (index <= self.colCount):
+            print('生成第 %d 列信息:' % (index))
+            str = input('  - 第 %d 列类型: ' % (index))
+            val = int(input('  - 第 %d 列起始数据(int): ' % (index)))
+            curCol = self.execOptList.create_col([str, val])
+            if (curCol == None):
+                print('列类型非法, 请重新输入')
+                continue
+            self.colList.append(curCol)
+
+            index += 1
+
+    def write_to_file(self, file, val):
+        file.write(val)
+
+    # TODO: 有些乱, 按逻辑拆分为几个函数
+    def main(self):
+        print('\n==================== main ====================')
+        filePath = self.execOptList.filePath + '.' + self.execOptList.rand
+        # 创建进程
+        bigen = 0
+        end = 0
+        for threadIdx in range(self.execOptList.threadCount):
+            end += self.rowCount // self.execOptList.threadCount
+            if end > self.rowCount or threadIdx == self.execOptList.threadCount - 1:
+                end = self.rowCount
+            self.fileList.append((filePath + '.' + str(threadIdx)))
+            if platform.architecture()[1] == 'ELF':
+                self.threadList.append(multiprocessing.Process( \
+                        target = self.process, \
+                        daemon = True, \
+                        args = [bigen, end, threadIdx]))
+            else:
+                self.threadList.append(threading.Thread( \
+                        target = self.process, \
+                        daemon = True, \
+                        args = [bigen, end, threadIdx]))
+            self.threadList[threadIdx].start()
+            print('\r  + 进程 %d / %d 开始' % (threadIdx + 1, self.execOptList.threadCount), end = '')
+            bigen = end
+        print('')
+        # 等待进程结束
+        for threadIdx in range(self.execOptList.threadCount):
+            self.threadList[threadIdx].join()
+            print('\r  - 进程 %d / %d 结束' % (threadIdx + 1, self.execOptList.threadCount), end = '')
+        # 合并文件
+        if (self.execOptList.needMergeFiles):
+            print('\n==================== marging ====================')
+            for threadIdx in range(self.execOptList.threadCount):
+                print('\r  - 正在合并第 %d / %d 份文件' % (threadIdx + 1, self.execOptList.threadCount), end = '')
+                # 判断系统类型, 选择不同合并方式
+                if platform.architecture()[1] == 'ELF':
+                    # 如果系统为Linux, 使用cat, 速度快
+                    command = str('cat ' + self.fileList[threadIdx] + ' >> ' + self.execOptList.filePath)
+                    os.system(command)
+                else:
+                    # 否则使用python, 速度慢
+                    with open(self.execOptList.filePath, mode = 'a', encoding = 'utf-8', newline='\n') as file:
+                        for line in open(self.fileList[threadIdx], encoding = 'utf-8', newline='\n'):
+                            file.writelines(line)
+                os.remove(self.fileList[threadIdx])
+        # 打乱文件行
+        if self.execOptList.needUpsetFiles:
+            print('\n\n==================== upseting ====================')
+            print('  - 正在打乱文件')
+            tmp_file = self.execOptList.filePath + '.' + self.execOptList.rand + '.upset'
+            readCount = 0
+            writeCount = 0
+            out = open(tmp_file, mode = 'w', encoding = 'utf-8', newline='\n')
+            lines=[]
+            with open(self.execOptList.filePath, mode = 'r', encoding = 'utf-8', newline='\n') as infile:
+                for line in infile:
+                    readCount += 1
+                    print('\r    - 读取文件行: %d' % readCount, end = '')
+                    lines.append(line)
+            random.shuffle(lines)
+            print('')
+            for line in lines:
+                writeCount += 1
+                print('\r    - 打乱文件行: %d / %d' % (writeCount, readCount), end = '')
+                out.write(line)
+            out.close()
+            os.remove(self.execOptList.filePath)
+            os.rename(tmp_file, self.execOptList.filePath)
+        print('\n\n==================== work all over ====================')
+
+
+    def process(self, bigen, end, threadIdx):
+        random.seed(self.execOptList.seed + threadIdx)
+        with open(self.fileList[threadIdx], mode = 'x', encoding = 'utf-8', newline='\n') as file:
+            curStr = str()
+            for index in range(bigen, end):
+                curStr = ''
+                curStr += self.colList[0].next(index)
+                for colidx in range(1, len(self.colList)):
+                    curStr += self.execOptList.separator
+                    curStr += self.colList[colidx].next(index)
+                curStr += '\n'
+                self.write_to_file(file, curStr)
+
+if __name__ == '__main__':
+    # info
+    print('create_csv_data v0.1.1')
+
+    # getopt
+    execOptList = func.execOpt()
+    execOptList.seed = execOptList.rand
+    random.seed(int(execOptList.seed))
+    print('\n当前随机数种子为: %s\n' % execOptList.rand)
+    options, args = getopt.getopt(sys.argv[1:], \
+            "hcmj:o:s:S:u", ["help", "use-config", "marge", "cpu=", "output=", "seed=", "separator=", "upset"])
+    try:
+        options, args = getopt.getopt(sys.argv[1:], \
+                "hcmj:o:s:S:u", ["help", "use-config", "marge", "cpu=", "output=", "seed=", "separator=", "upset"])
+    except getopt.GetoptError:
+        sys.exit()
+    for name, value in options:
+        if name in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif name in ("-c", "--use-config"):
+            execOptList.usingConfig = True
+        elif name in ("-m", "--marge"):
+            execOptList.needMergeFiles = True
+        elif name in ("-j", "--cpu"):
+            execOptList.threadCount = int(value)
+        elif name in ("-o", "--output"):
+            execOptList.filePath = value
+        elif name in ("-s", "--seed"):
+            execOptList.seed = int(value)
+            random.seed(execOptList.seed)
+            print('随机数种子设置为: %s\n' % value)
+        elif name in ("-S", "--separator"):
+            execOptList.separator = value
+        elif name in ("-r", "--shuffle"):
+            execOptList.needMergeFiles = True
+            execOptList.needShufFiles = True
+        else:
+            usage()
+            sys.exit()
+
+
+    # main
+    main_process = craete(execOptList)
+    main_process.main()
+
